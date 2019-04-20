657f717b0c93ab7b0a37d1214267fab12b93705c
ksavukov <37810935+ksavukov@users.noreply.github.com>
2019-04-20 22:39:56 +0300

    Do package

---
+++ sandbox/src/main/java/ru/qa/pft/sandbox/Hello.java
@@ -0,0 +1,13 @@
+package ru.qa.pft.sandbox;/* Name it "ru.qa.pft.sandbox.Hello.java"
+ * Compile it by running "javac ru.qa.pft.sandbox.Hello.java"
+ * Run it by typing "java ru.qa.pft.sandbox.Hello"
+ */
+
+class Hello {
+	public static void main(String args[])
+	{
+    	System.out.println("ru.qa.pft.sandbox.Hello, world!");
+	}
+}
+
+
